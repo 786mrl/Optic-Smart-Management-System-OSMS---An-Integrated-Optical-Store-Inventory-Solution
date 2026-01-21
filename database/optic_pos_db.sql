ef5ab4b09af345d437268c550fc3ab00fa87c471
muhammad_rais_latif <muhammadraislatif@gmail.com>
2026-01-21 22:37:46 +0700

    UPDATE NOW

--- database/optic_pos_db.sql
+++ database/optic_pos_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 19, 2026 at 02:12 PM
+-- Generation Time: Jan 21, 2026 at 04:37 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.2
 
@@ -42,8 +42,26 @@ CREATE TABLE `frames_main` (
   `sell_price` decimal(15,2) DEFAULT NULL,
   `price_secret_code` varchar(20) DEFAULT NULL,
   `stock` int(11) DEFAULT '0',
-  `stock_age` enum('very old','old','new') DEFAULT NULL
+  `stock_age` enum('very old','old','new') DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `frames_main`
+--
+
+INSERT INTO `frames_main` (`ufc`, `brand`, `frame_code`, `frame_size`, `color_code`, `material`, `lens_shape`, `structure`, `size_range`, `buy_price`, `sell_price`, `price_secret_code`, `stock`, `stock_age`, `created_at`, `updated_at`) VALUES
+('BOSS-E0419-54-17-140-C2', 'BOSS', 'E0419', '54-17-140', 'C2', 'B TITANIUM', 'AVIATOR', 'rimless', 'large', '66000.00', '320000.00', 'K20LZ', 5, 'very old', '2026-01-21 21:50:31', '2026-01-21 22:35:26'),
+('BRENDEN-brn-8654-50-18-138-col.1', 'BRENDEN', 'brn-8654', '50-18-138', 'col.1', 'PLASTIC', 'SQUARE', 'full-rim', 'medium', '20000.00', '90000.00', 'H40LZ', 1, 'very old', '2026-01-21 22:03:11', '2026-01-21 22:03:11'),
+('BVLGARI-1303-49-17-138-col.4', 'BVLGARI', '1303', '49-17-138', 'col.4', 'PLASTIC', 'Oval', 'semi-rimless', 'small', '88000.00', '440000.00', 'KH00LZ', 4, 'new', '2026-01-21 21:50:31', '2026-01-21 22:16:08'),
+('CEVIRO-lz-786-00-00-786-col.1', 'CEVIRO', 'lz-786', '00-00-786', 'col.1', 'METAL', 'Aviator', 'full-rim', 'medium', '30000.00', '135000.00', 'I35LZ', 2, 'new', '2026-01-21 21:50:31', '2026-01-21 21:50:31'),
+('CHANEL-58472-52-16-145-c5', 'CHANEL', '58472', '52-16-145', 'c5', 'PLASTIC', 'Square', 'full-rim', 'medium', '68000.00', '340000.00', 'K40LZ', 1, 'new', '2026-01-21 21:50:31', '2026-01-21 21:50:31'),
+('HUMANSKULL-H1520-45-23-140-C2', 'HUMAN SKULL', 'H1520', '45-23-140', 'C2', 'PLASTIC', 'Oval', 'full-rim', 'medium', '38000.00', '175000.00', 'IH25LZ', 6, 'new', '2026-01-21 21:50:31', '2026-01-21 22:21:15'),
+('MARXSTUDIO-Mstm-161-50-18-C5', 'MARX STUDIO', 'Mst m-161', '50-18', 'C5', 'PLASTIC', 'Square', 'full-rim', 'medium', '105000.00', '630000.00', 'MI30LZ', 1, 'new', '2026-01-21 21:50:31', '2026-01-21 21:50:31'),
+('MISSMAGDA-m99-19-51-18-145-C6', 'MISSMAGDA', 'M99-19', '51-18-145', 'c6', 'METAL', 'SQUARE', 'full-rim', 'medium', '35000.00', '160000.00', 'IH10LZ', 2, 'new', '2026-01-21 21:50:31', '2026-01-21 21:50:31'),
+('RAIS-Umi-786-00-00-786-C1', 'RAIS', 'Umi-786', '00-00-786', 'C1', 'METAL', 'SQUARE', 'full-rim', 'medium', '120000.00', '760000.00', 'NIH10LZ', 1, 'new', '2026-01-21 21:50:31', '2026-01-21 21:50:31'),
+('TAKEYAMA-TAKE700-52-15-140-col.4', 'TAKEYAMA', 'TAKE 700', '52-15-140', 'col.4', 'METAL', 'SQUARE', 'semi-rimless', 'medium', '45000.00', '205000.00', 'IH15LZ', 6, 'old', '2026-01-21 21:50:31', '2026-01-21 22:16:08');
 
 -- --------------------------------------------------------
 
@@ -82,22 +100,6 @@ CREATE TABLE `frame_staging` (
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `frame_staging`
---
-
-INSERT INTO `frame_staging` (`ufc`, `brand`, `frame_code`, `frame_size`, `color_code`, `material`, `lens_shape`, `structure`, `size_range`, `buy_price`, `sell_price`, `price_secret_code`, `stock`, `stock_age`, `created_at`, `updated_at`) VALUES
-('BOSS-E0419-54-17-140-C2', 'BOSS', 'E0419', '54-17-140', 'C2', 'PLASTIC', 'Square', 'full-rim', 'medium', '56000.00', '270000.00', 'LZJH20', 1, 'new', '2026-01-17 03:45:43', '2026-01-17 03:45:43'),
-('BVLGARI-1303-49-17-138-col.4', 'BVLGARI', '1303', '49-17-138', 'col.4', 'PLASTIC', 'Oval', 'semi-rimless', 'small', '70000.00', '350000.00', 'LZKH00', 3, 'new', '2026-01-16 11:52:21', '2026-01-16 11:59:15'),
-('CEVIRO-lz-786-00-00-786-col.1', 'CEVIRO', 'lz-786', '00-00-786', 'col.1', 'METAL', 'Aviator', 'full-rim', 'medium', '30000.00', '135000.00', 'LZI35', 2, 'new', '2026-01-17 03:37:41', '2026-01-17 03:42:32'),
-('CHANEL-58472-52-16-145-c5', 'CHANEL', '58472', '52-16-145', 'c5', 'PLASTIC', 'Square', 'full-rim', 'medium', '68000.00', '340000.00', 'LZK40', 1, 'new', '2026-01-17 04:25:34', '2026-01-17 04:25:34'),
-('HUMANSKULL-H1520-45-23-140-C2', 'HUMAN SKULL', 'H1520', '45-23-140', 'C2', 'PLASTIC', 'Oval', 'full-rim', 'medium', '38000.00', '175000.00', 'LZIH25', 1, 'new', '2026-01-17 04:14:20', '2026-01-17 04:14:20'),
-('KELLYS-Kel53006-52-19-145-col.7', 'KELLYS', 'Kel53006', '52-19-145', 'col.7', 'B TITANIUM', 'SQUARE', 'full-rim', 'medium', '43000.00', '195000.00', 'LZIH45', 2, 'new', '2026-01-18 16:44:46', '2026-01-18 16:45:51'),
-('MARXSTUDIO-Mstm-161-50-18-C5', 'MARX STUDIO', 'Mst m-161', '50-18', 'C5', 'PLASTIC', 'Square', 'full-rim', 'medium', '105000.00', '630000.00', 'LZMI30', 1, 'new', '2026-01-16 12:06:22', '2026-01-16 12:06:22'),
-('RAIS-Umi-786-00-00-786-C1', 'RAIS', 'Umi-786', '00-00-786', 'C1', 'METAL', 'SQUARE', 'full-rim', 'medium', '600000.00', '0.00', 'LZ00', 1, 'new', '2026-01-18 16:43:32', '2026-01-18 16:43:32'),
-('TAKEYAMA-TAKE648-52-15-140-C5', 'TAKEYAMA', 'TAKE 648', '52-15-140', 'C5', 'METAL', 'Cat Eye', 'full-rim', 'medium', '0.00', '0.00', '', 1, 'new', '2026-01-17 03:36:35', '2026-01-19 12:32:11'),
-('Z-GENERATION-28631-50-23-137-C12', 'Z-GENERATION', '28631', '50-23-137', 'C12', 'METAL', 'Square', 'full-rim', 'medium', '56000.00', '270000.00', 'LZJH20', 1, 'new', '2026-01-17 03:43:44', '2026-01-17 03:43:44');
 
 -- --------------------------------------------------------
 
@@ -169,7 +171,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `username`, `password_hash`, `role`, `is_approved`, `created_at`) VALUES
-(1, 'LenZa786', '$2y$10$eWcCL/3wpJqP5FbsdTi/pOW4vlpYpTT1mALlwOuykjumN7eP5OlhW', 'admin', 1, '2026-01-12 05:15:58'),
+(1, 'LenZa786', '$2y$10$E5ZXU41IpXcB443wtKCIou/cpEaFMa7k2tuOx83ZAQ9soeUPagGWm', 'admin', 1, '2026-01-12 05:15:58'),
 (16, 'Rais786', '$2y$10$oytb0PrQF9VUXlVjV8B9eu.1OEYFDIKXO8GqkDDVVZbcATJmteHIu', 'staff', 1, '2026-01-18 14:08:35');
 
 --
